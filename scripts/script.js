113a5c99c6e08be79f39a4fec47c5b08760528cd
Egor <vlesueji@gmail.com>
2025-06-17 19:13:54 +0300

    add mobile menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,14 @@
-let buttonMenu = document.getElementById('menuButton');
+const buttonMenu = document.getElementById('menuButton');
+const mobileMenu = document.getElementById('mobileMenu')
+
 let isOpened = false;
 
 let openMenu = function (){
     buttonMenu.classList.toggle('burger-button');
     buttonMenu.classList.toggle('burger-button-close');
+    mobileMenu.classList.toggle('hidden-mobile');
+    mobileMenu.classList.toggle('mobile-visible');
     isOpened = !isOpened;
 };
 buttonMenu.addEventListener('click', openMenu);
+
