ea45f6ac832eb71558a0789df77ed5a7bf0ceda2
Egor <vlesueji@gmail.com>
2025-06-18 21:06:58 +0300

    fix mobile menu

--- scripts/script.js
+++ scripts/script.js
@@ -6,8 +6,8 @@ let isOpened = false;
 let openMenu = function (){
     buttonMenu.classList.toggle('burger-button');
     buttonMenu.classList.toggle('burger-button-close');
-    mobileMenu.classList.toggle('hidden-mobile');
     mobileMenu.classList.toggle('mobile-visible');
+    mobileMenu.classList.toggle('hidden-mobile');
     isOpened = !isOpened;
 };
 buttonMenu.addEventListener('click', openMenu);
